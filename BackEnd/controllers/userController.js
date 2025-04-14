16bee78077721d9fb67d10da9e7559f0c670a566
chri-m <chrismaharjan.412@gmail.com>
2025-04-14 20:28:24 +0545

    Auth middleware added
    for protected get user data function

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -79,8 +79,22 @@ const getAllUsers = async (req, res) => {
     }
 }
 
+const getUser = async (req, res) => {
+    try {
+        userId = req.user._id; // Extract user ID from the request object
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error });
+    }
+}
+
 module.exports = {
     registerUser,
     loginUser,
     getAllUsers,
+    getUser,
 };
--- BackEnd/routes/userRoutes.js
+++ BackEnd/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser, getAllUsers } = require("../controllers/userController");
+const { registerUser, loginUser, getAllUsers, getUser } = require("../controllers/userController");
+const { protect } = require("../middlewares/authMiddleware");
 const userRouter = express.Router();
 
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/all", getAllUsers);
+userRouter.get("/user", protect, getUser);
 
 module.exports = userRouter;
